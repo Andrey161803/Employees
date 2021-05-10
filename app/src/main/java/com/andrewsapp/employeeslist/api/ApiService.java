75e26c95ba23e63a3e683bf1463d1d5c8943a491
Andrew <1618sar@gmail.com>
2021-05-10 21:06:04 +0400

    add ApiService

---
+++ app/src/main/java/com/andrewsapp/employeeslist/api/ApiService.java
@@ -0,0 +1,12 @@
+package com.andrewsapp.employeeslist.api;
+
+import com.andrewsapp.employeeslist.pojo.DataModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+interface ApiService {
+
+    @GET("v2/5ddcd3673400005800eae483")
+    Call<DataModel> getDataModel();
+}
