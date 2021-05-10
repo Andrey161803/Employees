9c68c1dbd072822eb407c04ce63ca44dc5e2babf
Andrew <1618sar@gmail.com>
2021-05-10 21:27:09 +0400

    create retrofit object and Callback<DataModel>

--- app/src/main/java/com/andrewsapp/employeeslist/MainActivity.java
+++ app/src/main/java/com/andrewsapp/employeeslist/MainActivity.java
@@ -1,14 +1,53 @@
 package com.andrewsapp.employeeslist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.andrewsapp.employeeslist.api.ApiService;
+import com.andrewsapp.employeeslist.pojo.DataModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "http://www.mocky.io/";
+    RecyclerView mRecyclerView;
+
+    private void init() {
+        mRecyclerView = findViewById(R.id.rvEmployees);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<DataModel> mCompanyCall = apiService.getDataModel();
+
+        mCompanyCall.enqueue(new Callback<DataModel>() {
+            @Override
+            public void onResponse(Call<DataModel> call, Response<DataModel> response) {
+                Log.d("RetrofitCall", "Success!");
+            }
+
+            @Override
+            public void onFailure(Call<DataModel> call, Throwable t) {
+                Log.d("RetrofitCall", "" +t.toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/andrewsapp/employeeslist/api/ApiService.java
+++ app/src/main/java/com/andrewsapp/employeeslist/api/ApiService.java
@@ -5,7 +5,7 @@ import com.andrewsapp.employeeslist.pojo.DataModel;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-interface ApiService {
+public interface ApiService {
 
     @GET("v2/5ddcd3673400005800eae483")
     Call<DataModel> getDataModel();
