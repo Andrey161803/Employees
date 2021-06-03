7d97723ff4288bf002baf0d732d573864174d24f
Andrew <1618sar@gmail.com>
2021-06-03 19:48:33 +0400

    add interface EmployessInfoDao

---
+++ app/src/main/java/com/andrewsapp/employeeslist/database/EmployeesInfoDao.java
@@ -0,0 +1,29 @@
+package com.andrewsapp.employeeslist.database;
+
+import com.andrewsapp.employeeslist.pojo.Employee;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+/*если телефонный номер сотрудника не будет null
+  его можно использовать как PrimaryKey
+  и перезаписывать только изминившиеся
+  @Insert (onConflict = OnConflictStrategy.REPLACE),
+  а так придётся удалять все записи и записывать снова.
+  можно сравнивать, но в данном случае наверное это не нужно*/
+
+@Dao
+interface EmployeesInfoDao {
+
+    @Query("SELECT * FROM employee_list")
+    LiveData<List<Employee>> getEmployees();
+
+    @Insert
+    void isertEmployees(List<Employee> employees);
+
+    @Query("DELETE FROM employee_list")
+    void deleteAllEmployees();
+}
